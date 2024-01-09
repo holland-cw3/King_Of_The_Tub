85d8bc374647dc52851910961325d3c966abcebb
holland-cw3 <Hollandcw3@gmail.com>
2024-01-09 14:51:52 -0500

    Update MainMenu.cs

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //Screen or button on click switches to scene
    public void switchScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
